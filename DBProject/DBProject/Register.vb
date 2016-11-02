1fb1b1a8b4ab600366b132fb38630e5697f9f3a2
Nathan Ell <elln2@mymacewan.ca>
2016-11-01 22:15:57 -0600

    Start registration screen

---
+++ DBProject/DBProject/Register.vb
@@ -0,0 +1,21 @@
+﻿Public Class Register
+    Private user As User = Nothing
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles SubmitButton.Click
+        ' Delete the old user if it existed
+        user = Nothing
+
+
+    End Sub
+
+    Private Sub Register_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If Not user Is Nothing Then
+            Me.UserNameBox.Text = user.getUsername()
+            Me.UserPassBox.Text = user.getPassword()
+        End If
+    End Sub
+
+    Public Sub SetUser(user As User)
+        Me.user = user
+    End Sub
+End Class
