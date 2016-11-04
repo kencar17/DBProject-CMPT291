3e9a8be55ab566652e2b4d6a6373c10ba32923b5
Nathan Ell <elln2@mymacewan.ca>
2016-11-03 21:25:53 -0600

    complete add new user functionality

--- DBProject/DBProject/AddNewUser.vb
+++ DBProject/DBProject/AddNewUser.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Runtime.InteropServices
+Imports MySql.Data.MySqlClient
 
 Public Class AddNewUser
     Private callingForm As UserAdmin
@@ -32,28 +33,101 @@ Public Class AddNewUser
     End Function
 
     Private Sub SubmitButton_Click(sender As Object, e As EventArgs) Handles SubmitButton.Click
-        ' 2. Ensure a password is given
-        ' 3. Create an Employee with given info
-        ' 4. Select the new Employee to get EID
         ' 5. Create a User with the given info
         Me.ErrorLabel.Visible = False
-        If User.FindUser(Me.UsernameBox.Text) Is Nothing Then
+
+        If EmailBox.Text.Equals("") Then
+            ErrorLabel.Text = "An email must be given."
+            Me.ErrorLabel.Visible = True
+        End If
+        If LastnameBox.Text.Equals("") Then
+            ErrorLabel.Text = "A last name must be given."
+            Me.ErrorLabel.Visible = True
+        End If
+        If FirstnameBox.Text.Equals("") Then
+            ErrorLabel.Text = "A first name must be given."
+            Me.ErrorLabel.Visible = True
+        End If
+        If PassBox.Text.Equals("") Then
+            ErrorLabel.Text = "A password must be given."
+            Me.ErrorLabel.Visible = True
+        End If
+        If UsernameBox.Text.Equals("") Then
+            ErrorLabel.Text = "A username must be given."
+            Me.ErrorLabel.Visible = True
+        End If
+        If Not User.FindUser(Me.UsernameBox.Text) Is Nothing Then
             ErrorLabel.Text = String.Format("{0} already exists", UsernameBox.Text)
             ErrorLabel.Visible = True
             Return
         End If
 
-        If UsernameBox.Text.Equals("") Then
-            ErrorLabel.Text = "A username must be given."
-        End If
-        If PassBox.Text.Equals("") Then
-            ErrorLabel.Text = "A password must be given."
-        End If
-        If FirstnameBox.Text.Equals("") Then
-            ErrorLabel.Text = "A first name must be given."
-        End If
-        If LastnameBox.Text.Equals("") Then
-            ErrorLabel.Text = "A last name must be given."
-        End If
+        Dim hashedPass As String = BCrypt.Net.BCrypt.HashPassword(PassBox.Text)
+
+        Dim dbconn As MySqlConnection = SQLConnection.Instance.GetConnection()
+        Using sqlComm As New MySqlCommand()
+            With sqlComm
+                .Connection = dbconn
+                .CommandText = "INSERT INTO Employee (FirstName, LastName, PostalCode, StreetAddress, City, State, Country, Email) VALUES (@firstname, @lastname, @pcode, @addr, @city, @state, @country, @email)"
+                .CommandType = CommandType.Text
+                .Parameters.AddWithValue("@firstname", FirstnameBox.Text)
+                .Parameters.AddWithValue("@lastname", LastnameBox.Text)
+                .Parameters.AddWithValue("@pcode", PostcodeBox.Text)
+                .Parameters.AddWithValue("@addr", AddressBox.Text)
+                .Parameters.AddWithValue("@city", CityBox.Text)
+                .Parameters.AddWithValue("@state", StateBox.Text)
+                .Parameters.AddWithValue("@country", CountryBox.Text)
+                .Parameters.AddWithValue("@email", EmailBox.Text)
+            End With
+            sqlComm.ExecuteNonQuery()
+        End Using
+        SQLConnection.Instance.CloseConnection()
+
+        dbconn = SQLConnection.Instance.GetConnection()
+        Dim eid As String
+        Using sqlComma As New MySqlCommand()
+            With sqlComma
+                .Connection = dbconn
+                .CommandText = "SELECT EID FROM Employee WHERE FirstName = @firstname AND LastName = @lastname AND Email = @email"
+                .CommandType = CommandType.Text
+                .Parameters.AddWithValue("@firstname", FirstnameBox.Text)
+                .Parameters.AddWithValue("@lastname", LastnameBox.Text)
+                .Parameters.AddWithValue("@email", EmailBox.Text)
+            End With
+            Try
+                Dim sqlReader As MySqlDataReader = sqlComma.ExecuteReader()
+                While sqlReader.Read()
+                    eid = sqlReader("EID").ToString()
+                End While
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        End Using
+        SQLConnection.Instance.CloseConnection()
+
+        dbconn = SQLConnection.Instance.GetConnection()
+        Console.WriteLine("~~~~~EID is " & eid)
+        Console.WriteLine("~~~~~Username is " & UsernameBox.Text)
+        Console.WriteLine("~~~~~Pass is " & hashedPass)
+        Using sqlCommb As New MySqlCommand()
+            With sqlCommb
+                .Connection = dbconn
+                .CommandText = "INSERT INTO User (Username, Password, PersonID) VALUES (@username, @pass, @eid)"
+                .CommandType = CommandType.Text
+                .Parameters.AddWithValue("@username", UsernameBox.Text)
+                .Parameters.AddWithValue("@pass", hashedPass)
+                .Parameters.AddWithValue("@eid", eid)
+            End With
+            Try
+                sqlCommb.ExecuteNonQuery()
+            Catch ex As Exception
+                Console.WriteLine("~~~~~Error was " & ex.Message)
+                Console.WriteLine(ex)
+            End Try
+        End Using
+        SQLConnection.Instance.CloseConnection()
+
+        MsgBox("User added!")
+        Me.Close()
     End Sub
 End Class
