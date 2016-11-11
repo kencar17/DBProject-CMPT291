cea719780a5a7a76095c8f5385bb7ff84a0c32cd
Nathan Ell <elln2@mymacewan.ca>
2016-11-10 23:08:22 -0700

    use github url

--- DBProject/DBProject/Help.vb
+++ DBProject/DBProject/Help.vb
@@ -3,12 +3,10 @@ Imports DBProject.My.Resources
 
 Public Class Help
     Public Shared Sub GetHelp(Optional section As String = Nothing)
-        Dim cwd As String = Directory.GetCurrentDirectory()
-        Dim url As String = "file://" & cwd & "\Help\index.html"
+        Dim url As String = "https://elln2.github.io/DBProject/"
         If Not section Is Nothing Then
-            url = url & "#" & section
+            url = url & section
         End If
-        Console.WriteLine(url)
         Process.Start(url)
     End Sub
 End Class
