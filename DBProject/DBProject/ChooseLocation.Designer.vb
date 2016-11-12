1a9c3ffef8a58678dbfa5460fee3c97008276efc
kencar17 <carmichaelk3@mymacewan.ca>
2016-11-12 16:00:30 -0700

    Finish screens for creating a rental

--- DBProject/DBProject/ChooseLocation.Designer.vb
+++ DBProject/DBProject/ChooseLocation.Designer.vb
@@ -24,16 +24,16 @@ Partial Class ChooseLocation
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.ComboBox1 = New System.Windows.Forms.ComboBox()
-        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.DateTimePicker2 = New System.Windows.Forms.DateTimePicker()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.ComboBox2 = New System.Windows.Forms.ComboBox()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.ComboBox3 = New System.Windows.Forms.ComboBox()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
+        Me.MonthCalendar1 = New System.Windows.Forms.MonthCalendar()
+        Me.MonthCalendar2 = New System.Windows.Forms.MonthCalendar()
         Me.SuspendLayout()
         '
         'Label1
@@ -49,17 +49,10 @@ Partial Class ChooseLocation
         '
         Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(252, 47)
+        Me.ComboBox1.Location = New System.Drawing.Point(235, 47)
         Me.ComboBox1.Name = "ComboBox1"
         Me.ComboBox1.Size = New System.Drawing.Size(179, 32)
         Me.ComboBox1.TabIndex = 1
-        '
-        'DateTimePicker1
-        '
-        Me.DateTimePicker1.Location = New System.Drawing.Point(69, 140)
-        Me.DateTimePicker1.Name = "DateTimePicker1"
-        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 29)
-        Me.DateTimePicker1.TabIndex = 2
         '
         'Label2
         '
@@ -73,23 +66,16 @@ Partial Class ChooseLocation
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(319, 102)
+        Me.Label3.Location = New System.Drawing.Point(479, 102)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(91, 25)
         Me.Label3.TabIndex = 4
         Me.Label3.Text = "Drop Off:"
         '
-        'DateTimePicker2
-        '
-        Me.DateTimePicker2.Location = New System.Drawing.Point(319, 140)
-        Me.DateTimePicker2.Name = "DateTimePicker2"
-        Me.DateTimePicker2.Size = New System.Drawing.Size(200, 29)
-        Me.DateTimePicker2.TabIndex = 5
-        '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(69, 193)
+        Me.Label4.Location = New System.Drawing.Point(69, 444)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(62, 25)
         Me.Label4.TabIndex = 6
@@ -98,7 +84,7 @@ Partial Class ChooseLocation
         'ComboBox2
         '
         Me.ComboBox2.FormattingEnabled = True
-        Me.ComboBox2.Location = New System.Drawing.Point(69, 233)
+        Me.ComboBox2.Location = New System.Drawing.Point(74, 478)
         Me.ComboBox2.Name = "ComboBox2"
         Me.ComboBox2.Size = New System.Drawing.Size(190, 32)
         Me.ComboBox2.TabIndex = 7
@@ -106,7 +92,7 @@ Partial Class ChooseLocation
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(319, 193)
+        Me.Label5.Location = New System.Drawing.Point(479, 444)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(62, 25)
         Me.Label5.TabIndex = 8
@@ -115,14 +101,14 @@ Partial Class ChooseLocation
         'ComboBox3
         '
         Me.ComboBox3.FormattingEnabled = True
-        Me.ComboBox3.Location = New System.Drawing.Point(319, 233)
+        Me.ComboBox3.Location = New System.Drawing.Point(484, 478)
         Me.ComboBox3.Name = "ComboBox3"
         Me.ComboBox3.Size = New System.Drawing.Size(195, 32)
         Me.ComboBox3.TabIndex = 9
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(385, 376)
+        Me.Button1.Location = New System.Drawing.Point(709, 575)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(128, 39)
         Me.Button1.TabIndex = 10
@@ -131,28 +117,40 @@ Partial Class ChooseLocation
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(69, 376)
+        Me.Button2.Location = New System.Drawing.Point(74, 575)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(96, 39)
         Me.Button2.TabIndex = 11
         Me.Button2.Text = "Back"
         Me.Button2.UseVisualStyleBackColor = True
         '
+        'MonthCalendar1
+        '
+        Me.MonthCalendar1.Location = New System.Drawing.Point(74, 138)
+        Me.MonthCalendar1.Name = "MonthCalendar1"
+        Me.MonthCalendar1.TabIndex = 12
+        '
+        'MonthCalendar2
+        '
+        Me.MonthCalendar2.Location = New System.Drawing.Point(484, 138)
+        Me.MonthCalendar2.Name = "MonthCalendar2"
+        Me.MonthCalendar2.TabIndex = 13
+        '
         'ChooseLocation
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 24.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(706, 473)
+        Me.ClientSize = New System.Drawing.Size(923, 709)
+        Me.Controls.Add(Me.MonthCalendar2)
+        Me.Controls.Add(Me.MonthCalendar1)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.ComboBox3)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.ComboBox2)
         Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.DateTimePicker2)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.DateTimePicker1)
         Me.Controls.Add(Me.ComboBox1)
         Me.Controls.Add(Me.Label1)
         Me.Name = "ChooseLocation"
@@ -164,14 +162,14 @@ Partial Class ChooseLocation
 
     Friend WithEvents Label1 As Label
     Friend WithEvents ComboBox1 As ComboBox
-    Friend WithEvents DateTimePicker1 As DateTimePicker
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
-    Friend WithEvents DateTimePicker2 As DateTimePicker
     Friend WithEvents Label4 As Label
     Friend WithEvents ComboBox2 As ComboBox
     Friend WithEvents Label5 As Label
     Friend WithEvents ComboBox3 As ComboBox
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
+    Friend WithEvents MonthCalendar1 As MonthCalendar
+    Friend WithEvents MonthCalendar2 As MonthCalendar
 End Class
