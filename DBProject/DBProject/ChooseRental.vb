cdfcd4684fa8645a3bf676035b078929a8588e57
kencar17 <carmichaelk3@mymacewan.ca>
2016-11-14 13:42:56 -0700

    Populate dropdowns

--- DBProject/DBProject/ChooseRental.vb
+++ DBProject/DBProject/ChooseRental.vb
@@ -1,4 +1,5 @@
-﻿Public Class ChooseRental
+﻿Imports MySql.Data.MySqlClient
+Public Class ChooseRental
     Private callingForm As ChooseLocation
 
     Public WriteOnly Property CallingFormProperty As ChooseLocation
@@ -21,5 +22,58 @@
     Public Sub doAThing()
         Hide()
         callingForm.doAThing()
+    End Sub
+
+    Private Sub seatsCombo_SelectedIndexChanged(sender As Object, e As EventArgs) Handles seatsCombo.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub ChooseRental_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For seat As Integer = 1 To 4
+            seatsCombo.Items.Add(seat)
+        Next
+
+        typeCombo.Items.Add("All")
+        Dim dbconn As MySqlConnection = SQLConnection.Instance.GetConnection()
+        Using sqlComm As New MySqlCommand()
+            With sqlComm
+                .Connection = dbconn
+                .CommandText = "SELECT Type FROM Types"
+                .CommandType = CommandType.Text
+            End With
+            Try
+                Dim sqlReader As MySqlDataReader = sqlComm.ExecuteReader()
+                While sqlReader.Read()
+                    Dim Type As String = sqlReader("Type").ToString()
+                    typeCombo.Items.Add(Type)
+                End While
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        End Using
+        SQLConnection.Instance.CloseConnection()
+        typeCombo.SelectedItem = "All"
+
+        makeCombo.Items.Add("All")
+        dbconn = SQLConnection.Instance.GetConnection()
+        Using sqlComm As New MySqlCommand()
+            With sqlComm
+                .Connection = dbconn
+                .CommandText = "SELECT DISTINCT Make FROM Vehicle"
+                .CommandType = CommandType.Text
+            End With
+            Try
+                Dim sqlReader As MySqlDataReader = sqlComm.ExecuteReader()
+                While sqlReader.Read()
+                    Dim Inventory As String = sqlReader("Make").ToString()
+                    makeCombo.Items.Add(Inventory)
+                End While
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        End Using
+        SQLConnection.Instance.CloseConnection()
+        makeCombo.SelectedItem = "All"
+
     End Sub
 End Class
